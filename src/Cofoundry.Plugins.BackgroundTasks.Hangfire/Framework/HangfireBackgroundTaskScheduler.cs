d3291aa25db2a718fc223b06c5fa8a066cbb551c
Joel Mitchell <me@joelmmitchell.co.uk>
2016-10-02 14:25:28 +0100

    Added async recurring task support

--- src/Cofoundry.Plugins.BackgroundTasks.Hangfire/Framework/HangfireBackgroundTaskScheduler.cs
+++ src/Cofoundry.Plugins.BackgroundTasks.Hangfire/Framework/HangfireBackgroundTaskScheduler.cs
@@ -26,7 +26,6 @@ namespace Cofoundry.Plugins.BackgroundTasks.Hangfire
             RegisterRecurringTask<TTask>(cronExpression);
 
             return this;
-
         }
 
         public IBackgroundTaskScheduler RegisterRecurringTask<TTask>(int minutes) where TTask : IRecurringBackgroundTask
@@ -38,6 +37,37 @@ namespace Cofoundry.Plugins.BackgroundTasks.Hangfire
         }
 
         public IBackgroundTaskScheduler DeregisterRecurringTask<TTask>() where TTask : IRecurringBackgroundTask
+        {
+            RecurringJob.RemoveIfExists(GetJobId<TTask>());
+
+            return this;
+        }
+
+        public IBackgroundTaskScheduler RegisterAsyncRecurringTask<TTask>(int days, int atHour = 0, int atMinute = 0) where TTask : IAsyncRecurringBackgroundTask
+        {
+            string cronExpression = string.Format("{2} {1} */{0} * *", days, atHour, atMinute);
+            RegisterAsyncRecurringTask<TTask>(cronExpression);
+
+            return this;
+        }
+
+        public IBackgroundTaskScheduler RegisterAsyncRecurringTask<TTask>(int hours = 0, int minute = 0) where TTask : IAsyncRecurringBackgroundTask
+        {
+            string cronExpression = string.Format("{1} */{0} * * *", hours, minute);
+            RegisterAsyncRecurringTask<TTask>(cronExpression);
+
+            return this;
+        }
+
+        public IBackgroundTaskScheduler RegisterAsyncRecurringTask<TTask>(int minutes) where TTask : IAsyncRecurringBackgroundTask
+        {
+            string cronExpression = string.Format("*/{0} * * * *", minutes);
+            RegisterAsyncRecurringTask<TTask>(cronExpression);
+
+            return this;
+        }
+
+        public IBackgroundTaskScheduler DeregisterAsyncRecurringTask<TTask>() where TTask : IAsyncRecurringBackgroundTask
         {
             RecurringJob.RemoveIfExists(GetJobId<TTask>());
 
@@ -58,6 +88,11 @@ namespace Cofoundry.Plugins.BackgroundTasks.Hangfire
             RecurringJob.AddOrUpdate<InjectableTaskWrapper<TTask>>(GetJobId<TTask>(), t => t.Execute(), cronExpression);
         }
 
+        private void RegisterAsyncRecurringTask<TTask>(string cronExpression) where TTask : IAsyncRecurringBackgroundTask
+        {
+            RecurringJob.AddOrUpdate<InjectableAsyncTaskWrapper<TTask>>(GetJobId<TTask>(), t => t.ExecuteAsync(), cronExpression);
+        }
+        
         #endregion
     }
 }
---
+++ src/Cofoundry.Plugins.BackgroundTasks.Hangfire/Framework/InjectableAsyncTaskWrapper.cs
@@ -0,0 +1,51 @@
+﻿
+using Hangfire;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cofoundry.Core.BackgroundTasks;
+using Cofoundry.Core.DependencyInjection;
+using Cofoundry.Core.ErrorLogging;
+
+namespace Cofoundry.Plugins.BackgroundTasks.Hangfire
+{
+    /// <summary>
+    /// Used to wrap task execution in a dispoable DI child context since
+    /// hangfire does not yet support child contexts.
+    /// </summary>
+    /// <typeparam name="TTask">Type fo task to execute</typeparam>
+    public class InjectableAsyncTaskWrapper<TTask> where TTask : IAsyncBackgroundTask
+    {
+        private readonly IResolutionContext _resolutionContext;
+
+        public InjectableAsyncTaskWrapper(
+            IResolutionContext resolutionContext
+            )
+        {
+            _resolutionContext = resolutionContext;
+        }
+
+        [AutomaticRetry(Attempts = 0)]
+        public async Task ExecuteAsync()
+        {
+            using (var childContext = _resolutionContext.CreateChildContext())
+            {
+                try
+                {
+                    var task = childContext.Resolve<TTask>();
+                    await task.ExecuteAsync();
+                }
+                catch (Exception ex)
+                {
+                    // I couldn't get hangfire error logging to work so I'm just catching any exceptions here
+                    // and sending them to the error logger.
+                    var errorLoggingService = childContext.Resolve<IErrorLoggingService>();
+                    errorLoggingService.Log(ex);
+
+                    throw;
+                }
+            }
+        }
+    }
+}
