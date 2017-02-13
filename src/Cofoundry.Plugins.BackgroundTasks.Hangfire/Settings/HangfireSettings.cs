04fe12b4f9a74fd36045f44a5c2d127570207147
Joel Mitchell <joel.mitchell@uberdigital.com>
2017-02-13 13:38:13 +0000

    - Added a README.md and docs - Updated packages

--- src/Cofoundry.Plugins.BackgroundTasks.Hangfire/Settings/HangfireSettings.cs
+++ src/Cofoundry.Plugins.BackgroundTasks.Hangfire/Settings/HangfireSettings.cs
@@ -7,9 +7,21 @@ using System.Threading.Tasks;
 
 namespace Cofoundry.Plugins.BackgroundTasks.Hangfire
 {
+    /// <summary>
+    /// Settings for HangFire configuration
+    /// </summary>
     public class HangfireSettings : PluginConfigurationSettingsBase
     {
+        /// <summary>
+        /// Prevents the HangFire server being configuted and started. Defaults
+        /// to false.
+        /// </summary>
         public bool DisableHangfire { get; set; }
+
+        /// <summary>
+        /// Enables the HangFire dashboard for Cofoundry admin users 
+        /// at /admin/hangfire. Defaults to false.
+        /// </summary>
         public bool EnableHangfireDashboard { get; set; }
     }
 }
